1436fd17233cdfeace3108afa9295b8af926f60b
Yina Arenas <yina.arenas@gmail.com>
2009-10-21 09:35:14 -0400

    UI skeleton

---
+++ NanoSim/src/com/nanosim/gwt/client/Proposals.java
@@ -0,0 +1,22 @@
+package com.nanosim.gwt.client;
+
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Composite that represents a collection of <code>Task</code> items.
+ */
+public class Proposals extends Composite {
+
+  public Proposals() {
+    SimplePanel panel = new SimplePanel();
+    VerticalPanel list = new VerticalPanel();
+    panel.setWidget(list);
+    list.add(new CheckBox("TODO toolbox"));
+    initWidget(panel);
+    setStyleName("nanosim-Research");
+  }
+
+}
