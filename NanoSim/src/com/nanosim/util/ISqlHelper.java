2fdb8ea2e87bf4cea73d53c0b6b56e152e1cefc7
Tamal Saha <tamal.saha@gmail.com>
2009-11-30 13:37:46 -0500

    everything added

--- NanoSim/src/com/nanosim/util/ISqlHelper.java
+++ NanoSim/src/com/nanosim/util/ISqlHelper.java
@@ -3,11 +3,14 @@ package com.nanosim.util;
 import java.sql.ResultSet;
 
 public interface ISqlHelper {
-
+    
+	//select query
 	ResultSet executeQuery(String sql, Object... params) throws Exception;
 
+	//insert, delete, update query
 	int executeUpdate(String sql, Object... params) throws Exception;
 
+	//insert  query
 	Object executeUpdate(String sql, Object[] params, String idQuery, Object[] id) throws Exception;
 
 	void close();
